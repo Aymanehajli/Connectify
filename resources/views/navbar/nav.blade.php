a73a5f37a62c1f6fe009fccde3ed0c9e2eba80f7
aymane <hajliaymane@gmail.com>
2024-06-07 15:16:38 +0100

    zwaq v2

--- resources/views/navbar/nav.blade.php
+++ resources/views/navbar/nav.blade.php
@@ -153,7 +153,9 @@
                 @auth
                 <form class="form-inline my-2 my-lg-0 search-form" id="friendSearchForm" action="{{ route('friends.search') }}" method="GET">
                     <input class="form-control mr-sm-2" type="text" name="search" id="searchInput" placeholder=" Search" >
-                    <button class="btn btn-outline-success my-2 my-sm-0 btn-search" type="submit" id="searchButton">Search</button>
+                    <button class="btn btn-outline-success my-2 my-sm-0 btn-search" type="submit" id="searchButton"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
+  <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001q.044.06.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1 1 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0"/>
+</svg></button>
                 </form>
                 @endauth
   
